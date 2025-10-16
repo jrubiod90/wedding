836e68a588420c214d0de910a2c00ea3cf16d690
jorgerubiodiez <jorgerubiodiez@outlook.com>
2025-10-16 02:13:02 +0200

    first version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wedding-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to weddingEN-angular!');
   }));
 });
